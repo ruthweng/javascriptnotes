6d40e6189e0491434e5790d35f1f281577c3b07c
ruthweng <ruthweng333@gmail.com>
2023-11-02 13:15:34 -0400

    Update occupiedspaces.js

---
+++ occupiedspaces.js
@@ -0,0 +1,10 @@
+spaces(5, "CC..C", ".CC..");
+function spaces(s, y, t) {
+    let occupied = 0;
+    for(let i = 0; i < s; i++){
+        if(y[i] === "C" && t[i]==="C"){
+
+        }
+    }
+    console.log(occupied);
+}
